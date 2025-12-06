09a0a6dc110d82e9efd41dfe071ecfb5a64947ae
kietotran04-wq <kietotran04@gmail.com>
2025-12-06 08:54:55 +0700

    v2.0

---
+++ UPDATE_MODPACK.bat
@@ -0,0 +1,9 @@
+@echo off
+echo DANG KIEM TRA BAN CAP NHAT TU SERVER VELARIS...
+
+:: Đây mới là link đúng (Link Raw trỏ thẳng vào file pack.toml)
+java -jar packwiz-installer-bootstrap.jar "https://raw.githubusercontent.com/kietotran04-wq/Velaris-modpack/main/pack.toml"
+
+echo.
+echo CAP NHAT XONG! VAO GAME DI BRO.
+pause
---
+++ scripts/lightning_bottle_recipe.zs
@@ -0,0 +1,2 @@
+craftingTable.addShaped("bottled_lightning", <item:irons_spellbooks:lightning_bottle>, [
+    [<item:create:copper_nugget>, <item:minecraft:glass_bottle>, <item:create:copper_nugget>]]);
